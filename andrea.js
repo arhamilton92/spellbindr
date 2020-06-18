8bbebac56e7388cc617e52da0d2321b5ed79694f
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-06-18 13:53:37 -0400

    moved to new branch

--- andrea.js
+++ andrea.js
@@ -1,6 +1,19 @@
-queryURL = "https://www.dnd5eapi.co/api/";
-magicSchoolsURL = "https://www.dnd5eapi.co/api/magic-schools";
-pickupLineURL = "http://pebble-pickup.herokuapp.com/tweets/random"
+//VARIABLES
+var queryURL = "https://www.dnd5eapi.co/api/";
+var magicSchoolsURL = "https://www.dnd5eapi.co/api/magic-schools";
+var equipmentURL = "https://www.dnd5eapi.co/api/equipment/"
+var spellsURL = "https://www.dnd5eapi.co/api/spells/"
+
+var pickupLineURL = "http://pebble-pickup.herokuapp.com/tweets/random"
+
+var magicSchool = "";
+var equipment = "";
+var index = "";
+var index2 = "";
+var spell1 = "";
+var spell2 = "";
+var equipment1 = "";
+var equipment2 = "";
 
 var firstnameArray = [
     "Oloril", "Sharry", "Fruamros", "Graamros", "Beleak", "Kevben", "Gandalf", "Dumbledore", "Harry", 
@@ -13,7 +26,7 @@ var lastnameArray = ["Buttonwood", "Horn", "Cork", "Striker", "Drake", "Buttonwo
     "Swordhand",  "Fletcher",  "Pegason",  "Loyalar",  "Chorster",  "Urthadar"
 ]
 
-
+//TEMPORARY
 $.ajax({
     url: queryURL,
     method: "GET"
@@ -27,11 +40,13 @@ $.ajax({
 }).then(function(response) {
     console.log(response);
 })
+//
 
+//FUNCTION DEFINITIONS
 function generateName() {
     firstName = firstnameArray[Math.floor(Math.random() * firstnameArray.length)];
     lastName = lastnameArray[Math.floor(Math.random() * lastnameArray.length)];
-    console.log(firstName, lastName);
+    generateStats();
 }
 
 function generateStats() {
@@ -40,9 +55,45 @@ function generateStats() {
         method: "GET"
     }).then(function(response) {
         console.log(response);
-    })
+    index = Math.floor(Math.random() * 8)
+    magicSchool = response.results[index].name;
+        
+        $.ajax({
+            url: equipmentURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+        index = Math.floor(Math.random() * 231)
+        index2 = Math.floor(Math.random() * 231)
+        equipment1 = response.results[index].name;
+        equipment2 = response.results[index2].name;
+        })
+            
+            $.ajax({
+                url: spellsURL,
+                method: "GET"
+            }).then(function(response) {
+                console.log(response);
+            index = Math.floor(Math.random() * 318)
+            index2 = Math.floor(Math.random() * 318)
+            spell1 = response.results[index].name;
+            spell2 = response.results[index2].name;
+            generateCharacter();
+            })
 
+    })
 }
 
+function generateCharacter() {
+    console.log(firstName, lastName);
+    console.log("Magic school: " + magicSchool);
+    console.log("equipment:");
+    console.log(equipment1);
+    console.log(equipment2);
+    console.log("spells:");
+    console.log(spell1);
+    console.log(spell2);
+}
+
+//FUNCTION CALLS
 generateName();
-generateStats();
