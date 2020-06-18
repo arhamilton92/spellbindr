15a31c36e1197e118b0fd933860f5f38744bab4b
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-06-18 12:40:21 -0400

    created first and last name arrays

--- andrea.js
+++ andrea.js
@@ -1,6 +1,18 @@
 queryURL = "https://www.dnd5eapi.co/api/";
 pickupLineURL = "http://pebble-pickup.herokuapp.com/tweets/random"
 
+var firstnameArray = [
+    "Oloril", "Sharry", "Fruamros", "Graamros", "Beleak", "Kevben", "Gandalf", "Dumbledore", "Harry", 
+    "Mervin", "Thicket", "Ruben", "Aedan", "Sorrel", "Wylie", "Briar", "Talon", "Ruben", "Gust", "Barney", 
+    "Leroy", "Fennel", "Quartz", "Wren", "Carnelian",
+]
+var lastnameArray = ["Buttonwood", "Horn", "Cork", "Striker", "Drake", "Buttonwood", "Roc", "Rooks", "Firethron", 
+    "Bovin", "Owler", "Juniper", "Raywood", "Foxglove", "Bells", "Snakebark", "Aurora", "Flame", "Fogs", 
+    "Tadpole", "Talpin", "Gorgon", "Owler", "Fiddlewood", "Locks", "Pegas", "Fluke", "Spindle", "Lambkins",
+    "Swordhand",  "Fletcher",  "Pegason",  "Loyalar",  "Chorster",  "Urthadar"
+]
+
+
 $.ajax({
     url: queryURL,
     method: "GET"
